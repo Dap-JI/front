57b9ac3ba96cf1390b88a17f0f86ec321200badf
jiyong <parkjiy96@naver.com>
2024-07-25 16:39:07 +0900

    feat : searchBar ui구현

--- src/app/climbList/page.tsx
+++ src/app/climbList/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import SearchBar from '@/src/components/common/searchBar';
+import classNames from 'classnames/bind';
+import styles from './ClimbListPage.module.scss';
+import FooterBar from '@/src/components/common/footerBar';
+
+const cn = classNames.bind(styles);
 
 const ClimbListPage = () => {
-  return <div>ClimbListPage</div>;
+  return (
+    <div className={cn('container')}>
+      <SearchBar />
+      <div>카드리스트 영역</div>
+      <FooterBar />
+    </div>
+  );
 };
 
 export default ClimbListPage;
