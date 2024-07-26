e0e7747c2c6d6dc571e7c3509fa2f7b8bbc531c3
jiyong <parkjiy96@naver.com>
2024-07-26 15:24:56 +0900

    feat : 카카오로그인 구현중

---
+++ src/app/auth/kakao/callback/page.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Kakao = () => {
+  // useEffect(() => {
+  //   const code = new URLSearchParams(window.location.search).get('code');
+  //   console.log(code);
+  // }, []);
+  return <div>Kakao</div>;
+};
+
+export default Kakao;
