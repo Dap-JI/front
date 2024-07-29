dc968535ffc92324e24b7e43a404ae521e7f6a88
jiyong <parkjiy96@naver.com>
2024-07-29 21:01:07 +0900

    feat :디테일페이지 구현

--- src/components/climbListDetailPage/detailMainContent/index.tsx
+++ src/components/climbListDetailPage/detailMainContent/index.tsx
@@ -4,7 +4,19 @@ import styles from './detailMainContent.module.scss';
 const cn = classNames.bind(styles);
 
 const DetailMainContent = () => {
-  return <div>DetailMainContent</div>;
+  return (
+    <div className={cn('container')}>
+      <div className={cn('videoWrapper')}>동영상 들어갈곳</div>
+      <div className={cn('contentWrapper')}>
+        <div className={cn('infoWrapper')}>
+          <span>{color}</span>
+          <span>{clrearday}</span>
+          <span>{usename}</span>
+        </div>
+        <p className={cn('mainWapper')}>{content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default DetailMainContent;
