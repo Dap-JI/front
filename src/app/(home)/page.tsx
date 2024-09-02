371df1075dc1cfd181e8a06a92707bc924eb87e7
jiyong <parkjiy96@naver.com>
2024-09-02 20:50:30 +0900

    #39 feat : 로그인 회원가입 페이지 간단구현

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
+'use client';
 import classNames from 'classnames/bind';
 import styles from './HomePage.module.scss';
 import OauthBtnForm from '@/src/components/loginPage/oauthBtnForm';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const cn = classNames.bind(styles);
 
 const Home = () => {
+  const router = useRouter();
   return (
     <div className={cn('container')}>
       <h1>Dap Ji</h1>
@@ -13,9 +15,9 @@ const Home = () => {
         <h2>답지 보러 가기 👇</h2>
         <OauthBtnForm />
       </div>
-      <Link href="/climbList">
-        <button>들어가기</button>
-      </Link>
+      <span className={cn('loginClick')} onClick={() => router.push(`/signin`)}>
+        🤫
+      </span>
     </div>
   );
 };
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import styles from './signinPage.module.scss';
+import classNames from 'classnames/bind';
+import CommonButton from '@/src/components/common/commonButton';
+import CommonInput from '@/src/components/common/commonInput';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import ModalChoice from '@/src/components/common/moadlChoice';
+import { useModal } from '@/src/hooks/useModal';
+import { fetchSignIn, fetchSignInType } from './api';
+import { useMutation } from '@tanstack/react-query';
+const cn = classNames.bind(styles);
+
+const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<fetchSignInType>();
+  const router = useRouter();
+  const { showModalHandler } = useModal();
+
+  const { mutate: userSignIn } = useMutation({
+    mutationKey: ['userSignIn'],
+    mutationFn: (formData: fetchSignInType) => fetchSignIn(formData),
+    onSuccess: (data: any) => {
+      if (data.user.user.nickname === null) {
+        router.push('/join');
+        return;
+      }
+      router.push(`/climbList`);
+    },
+    onError: () => {
+      showModalHandler('alert', '로그인 에러');
+    },
+  });
+
+  const onSubmit = (data: fetchSignInType) => {
+    const formData = {
+      ...data,
+    };
+    userSignIn(formData);
+  };
+
+  return (
+    <div className={cn('container')}>
+      <h1>로그인</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className={cn('signInForm')}>
+        <CommonInput
+          label="email"
+          register={register('email', {
+            required: '이메일 꼭 필요함',
+          })}
+        />
+        {errors.email && <span>{errors.email.message as string}</span>}
+        <CommonButton name="로그인" type="submit" />
+      </form>
+      <span onClick={() => router.push(`/signup`)}>회원가입 ㄱㄱ</span>
+      <span onClick={() => router.push(`/`)}>소셜로그인 ㄱㄱ</span>
+      <ModalChoice />
+    </div>
+  );
+};
+
+export default SignInPage;
