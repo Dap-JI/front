09531a97da36070e7fbb133afb8901bab461bbba
jiyong <parkjiy96@naver.com>
2024-09-05 16:32:49 +0900

    #39 feat : 팔로우 팔로잉 페이지 검색 구현

--- src/app/profile/[userId]/follow/page.tsx
+++ src/app/profile/[userId]/follow/page.tsx
@@ -1,74 +1,8 @@
-// 'use client';
-// import classNames from 'classnames/bind';
-// import styles from './followPage.module.scss';
-// import SearchBar from '@/src/components/common/searchBar';
-// import { fetchFollowerData, fetchFollowingData } from '@/src/app/profile/api';
-// import { useQuery } from '@tanstack/react-query';
-// import FollowAllData from '@/src/components/profilePage/followAllData';
-// import useScrollDirection from '@/src/hooks/useScrollDirection';
-// import { FollowerType, FollowingType } from '@/src/utils/type';
-
-// const cn = classNames.bind(styles);
-
-// type FollowerPageProps = {
-//   params: {
-//     userId: string;
-//   };
-// };
-
-// const FollowPage = ({ params }: FollowerPageProps) => {
-//   const { userId } = params;
-//   const [scrollDirection] = useScrollDirection('up');
-
-//   //팔로워 데이터
-//   const { data: followerDatas } = useQuery<FollowingType>({
-//     queryKey: ['followerDatas'],
-//     queryFn: () => fetchFollowerData(userId),
-//   });
-//   const followerData = followerDatas?.data ?? [];
-
-//   //팔로잉 데이터
-//   const { data: followingDatas } = useQuery<FollowerType>({
-//     queryKey: ['followingDatas'],
-//     queryFn: () => fetchFollowingData(userId),
-//   });
-//   const followingData = followingDatas?.data ?? [];
-
-//   const followObject = {
-//     followerObject: followerData,
-//     followingObject: followingData,
-//   };
-
-//   return (
-//     <div className={cn('container')}>
-//       <div
-//         className={cn('header-container', {
-//           up: scrollDirection === 'up',
-//           down: scrollDirection === 'down',
-//         })}
-//       >
-//         <SearchBar />
-//       </div>
-//       <div className={cn('secondContainer')}>
-//         <FollowAllData followObject={followObject} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FollowPage;
-
-// //팔로잉 팔로우 삭제에는 invalidate설정 안하기
-// //실수로 햇을수도 있으니까
-
-// //검색을 해야된까 페이지에서 데이터 객체로 묵어서 내려주기
-
-// //followAllData타입 지정해주기
-
 'use client';
 import classNames from 'classnames/bind';
 import styles from './followPage.module.scss';
 import FollowAllData from '@/src/components/profilePage/followAllData';
+import Header from '@/src/components/common/header';
 
 const cn = classNames.bind(styles);
 
@@ -81,6 +15,7 @@ type FollowerPageProps = {
 const FollowPage = ({ params }: FollowerPageProps) => {
   return (
     <div className={cn('container')}>
+      <Header back={true} />
       <div className={cn('secondContainer')}>
         <FollowAllData params={params} />
       </div>
@@ -89,10 +24,3 @@ const FollowPage = ({ params }: FollowerPageProps) => {
 };
 
 export default FollowPage;
-
-//팔로잉 팔로우 삭제에는 invalidate설정 안하기
-//실수로 햇을수도 있으니까
-
-//검색을 해야된까 페이지에서 데이터 객체로 묵어서 내려주기
-
-//followAllData타입 지정해주기
