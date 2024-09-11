c6dba56bfab553c9b5880e9612ffb470c6d7f5d0
yhs0329 <scar9983@gamil.com>
2024-09-11 17:10:33 +0900

    css 수정-naver 대문자, 클로잉 중, 클로우 하기 수정

--- src/components/profilePage/profileForm/index.tsx
+++ src/components/profilePage/profileForm/index.tsx
@@ -30,14 +30,14 @@ const ProfileForm = ({ params, profileInfo }: ProfileFormProps) => {
         return (
           <>
             <KakaoIcon width="17" height="17" />
-            <span>Kakao</span>
+            <span>KAKAO</span>
           </>
         );
       case 'naver':
         return (
           <>
             <NaverIcon width="30" height="30" />
-            <span>Naver</span>
+            <span>NAVER</span>
           </>
         );
       case 'dapji':
